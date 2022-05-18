1411f31bc80f8f8bcca75df77562a67db057c670
Reza Bagheri <rezq.bagheri@gmail.com>
2022-05-18 10:14:53 -0700

    Changed detection algorithm. Includes debounce timer but I might remove this later if it's deemed not necessary.

--- RoomLED.ino
+++ RoomLED.ino
@@ -7,7 +7,7 @@ const unsigned int LED_PIN = 6;
 const unsigned int PIXELS = 60;
 
 // Ticks per second for LED animations
-const int TPS = 1;
+const int TPS = 100;
 
 /* Define sonar constants */
 // Sonar Pins
@@ -20,35 +20,53 @@ const unsigned int SONAR_TWO_ECHO_PIN = 4;
 const int detectedThreshold = 110;
 const float secondsToEndScan = 5;
 
+// Debounce after walk in/out detection
+const int ticksToDebounce = 0;
+
 // Room state
 unsigned int numPeople = 0;
 
 /* States for state machine */
-enum action {
-  TURNING_ON = 0,
-  ON = 1,
-  TURNING_OFF = 2,
-  OFF = 3,
+enum action
+{
+    TURNING_ON = 0,
+    ON = 1,
+    TURNING_OFF = 2,
+    OFF = 3,
 };
 
-enum sonar {
-  BEFORE_DOOR = 0,
-  AFTER_DOOR = 1,
+enum sonar
+{
+    BEFORE_DOOR = 0,
+    AFTER_DOOR = 1,
 };
 
-typedef struct {
-  int red;
-  int green;
-  int blue;
+enum sonarState
+{
+    BEFORE_DETECTED = 0,
+    NONE_DETECTED = 1,
+    AFTER_DETECTED = 2,
+    BOTH_DETECTED = 3,
+};
+
+typedef struct
+{
+    int red;
+    int green;
+    int blue;
 } RGBColor;
 
-typedef struct {
-  RGBColor WHITE;
+typedef struct
+{
+    RGBColor WHITE;
 } colors;
 
 const colors color = {
-  .WHITE = (RGBColor) {255, 255, 255},
+    .WHITE = (RGBColor){255, 255, 255},
 };
+
+int turnOn[12] = {5, 10, 20, 30, 50, 70, 110, 150, 200, 225, 250, 255};
+int turnOff[12] = {255, 250, 225, 200, 150, 110, 70, 50, 30, 20, 10, 5};
 
 /* Calculate MS per tick for animations */
 const unsigned int MS_PER_TICK = 1000 / TPS;
@@ -62,185 +80,281 @@ enum action currentState;
 // Stores the last time a tick was done
 unsigned long lastTick = 0;
 
+// Debounce timer
+int debounce = 0;
+
 // Used for animations that require brightness
-unsigned int currentBrightness = 0;
+unsigned int currentAnimationTick = 0;
 
 // Used to detect walking in/out
-int heightDetected = 0;
-enum sonar sensorDetected = BEFORE_DOOR;
-unsigned long timeToEndScan = 0;
+sonarState sonarHistory[4] = {NONE_DETECTED, NONE_DETECTED, NONE_DETECTED, NONE_DETECTED};
+sonarState detectionHistory[3] = {NONE_DETECTED, NONE_DETECTED, NONE_DETECTED};
 
-void setup() {
-  // Sets up sonar pins
-  pinMode(SONAR_ONE_TRIG_PIN, OUTPUT);
-  pinMode(SONAR_ONE_ECHO_PIN, INPUT);
-  pinMode(SONAR_TWO_TRIG_PIN, OUTPUT);
-  pinMode(SONAR_TWO_ECHO_PIN, INPUT);
-  
-  // Resets LED pixels
-  pixels.begin();
-  pixels.clear();
+sonarState walkInPattern[4] = {BEFORE_DETECTED, NONE_DETECTED, AFTER_DETECTED, NONE_DETECTED};
+sonarState altWalkInPattern[4] = {NONE_DETECTED, BEFORE_DETECTED, AFTER_DETECTED, NONE_DETECTED};
 
-  pixels.setBrightness(0);
-  for (int i = 0; i < PIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(255, 255, 255));
-  }
+sonarState walkOutPattern[4] = {AFTER_DETECTED, NONE_DETECTED, BEFORE_DETECTED, NONE_DETECTED};
+sonarState altWalkOutPattern[4] = {NONE_DETECTED, AFTER_DETECTED, BEFORE_DETECTED, NONE_DETECTED};
 
-  // Initiates state machine
-  currentState = OFF;
+sonarState currentSonarState = NONE_DETECTED;
 
-  Serial.begin(9600);
-}
+void setup()
+{
+    // Sets up sonar pins
+    pinMode(SONAR_ONE_TRIG_PIN, OUTPUT);
+    pinMode(SONAR_ONE_ECHO_PIN, INPUT);
+    pinMode(SONAR_TWO_TRIG_PIN, OUTPUT);
+    pinMode(SONAR_TWO_ECHO_PIN, INPUT);
 
-void loop() {
-  
-  //pixels.clear();
-  //pixels.setBrightness(10);
-  //pixels.setPixelColor(0, pixels.Color(255, 255, 255));
-  //pixels.show();
+    // Resets LED pixels
+    pixels.begin();
 
-  unsigned long currentTime = millis();
+    pixels.clear();
 
-  // If enough time has passed for a new tick to happen, run the tick
-  if (currentTime - lastTick >= MS_PER_TICK) {
-    runPeriodic();
-    lastTick = currentTime;
-  }
-}
-
-void runPeriodic() {
-  enabledPeriodic();
-  switch (currentState) {
-    case TURNING_ON:
-      turningOnPeriodic();
-      break;
-    case TURNING_OFF:
-      turningOffPeriodic();
-      break;
-    case ON:
-      onPeriodic();
-      break;
-    case OFF:
-      offPeriodic();
-      break;
-  }
-}
-
-void enabledPeriodic() {
-  unsigned long currentTime = millis();
-  if (timeToEndScan < currentTime) {
-    // No item has triggered the detecting in/out process
-    int beforeDoorDistance = querySonar(BEFORE_DOOR);
-    int afterDoorDistance = querySonar(AFTER_DOOR);
-
-    if (beforeDoorDistance <= detectedThreshold) {
-      heightDetected = beforeDoorDistance;
-      sensorDetected = BEFORE_DOOR;
-      timeToEndScan = currentTime + (secondsToEndScan * 1000);
-    } else if (afterDoorDistance <= detectedThreshold) {
-      heightDetected = afterDoorDistance;
-      sensorDetected = AFTER_DOOR;
-      timeToEndScan = currentTime + (secondsToEndScan * 1000);
-    }
-  } else {
-    // Some item has triggered the detection of in/out process
-    int distance;
-    if (sensorDetected == BEFORE_DOOR) {
-      distance = querySonar(AFTER_DOOR);
-    } else {
-      distance = querySonar(BEFORE_DOOR);
+    pixels.setBrightness(0);
+    for (int i = 0; i < PIXELS; i++)
+    {
+        pixels.setPixelColor(i, pixels.Color(255, 255, 255));
     }
 
-    if (distance < heightDetected + 20 && distance > heightDetected - 20) {
-      if (sensorDetected == BEFORE_DOOR) {
-        walkIn();
-      } else {
-        walkOut();
-      }
-    }
-  }
-}
-
-void onPeriodic() {
-
-}
-
-void offPeriodic() {
-  
-}
-
-void turningOnPeriodic() {
-  unsigned int newBrightness = currentBrightness += 10;
-
-  if (newBrightness >= 255) {
-    newBrightness = 255;
-    currentState = ON;
-  }
-
-  pixels.setBrightness(newBrightness);
-  pixels.show();
-
-  currentBrightness = newBrightness;
-}
-
-
-void turningOffPeriodic() {
-  // Can't be unsigned since it might jump into the negatives
-  int newBrightness = currentBrightness -= 10;
-
-  if (newBrightness <= 0) {
-    newBrightness = 0;
+    // Initiates state machine
     currentState = OFF;
-  }
 
-  pixels.setBrightness(newBrightness);
-  pixels.show();
-
-  currentBrightness = newBrightness;
+    Serial.begin(9600);
 }
 
-int querySonar(enum sonar sensor) {
-  int trigPin;
-  int echoPin;
+void loop()
+{
 
-  if (sensor == BEFORE_DOOR) {
-    trigPin = SONAR_ONE_TRIG_PIN;
-    echoPin = SONAR_ONE_ECHO_PIN;
-  } else if (sensor == AFTER_DOOR) {
-    trigPin = SONAR_TWO_TRIG_PIN;
-    echoPin = SONAR_TWO_ECHO_PIN;
-  }
+    // pixels.clear();
+    // pixels.setBrightness(10);
+    // pixels.setPixelColor(0, pixels.Color(255, 255, 255));
+    // pixels.show();
 
-  // Clears the trigger
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  
-  // Triggers sonar
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  // Measure time for ping to return
-  unsigned long duration = pulseIn(echoPin, HIGH);
+    unsigned long currentTime = millis();
 
-  // Does the math!!! crazy hard LOL!!!
-  return duration * 0.034 / 2;
+    // If enough time has passed for a new tick to happen, run the tick
+    if (currentTime - lastTick >= MS_PER_TICK)
+    {
+        runPeriodic();
+        lastTick = currentTime;
+    }
+    // runPeriodic();
 }
 
-void walkIn() {
-  if (numPeople == 0) {
-    currentState = TURNING_ON;
-  }
-
-  numPeople++;
-  Serial.println("Someone walked in. Num people: " + numPeople);
+void runPeriodic()
+{
+    enabledPeriodic();
+    switch (currentState)
+    {
+    case TURNING_ON:
+        turningOnPeriodic();
+        break;
+    case TURNING_OFF:
+        turningOffPeriodic();
+        break;
+    case ON:
+        onPeriodic();
+        break;
+    case OFF:
+        offPeriodic();
+        break;
+    }
 }
 
-void walkOut() {
-  numPeople--;
-  Serial.println("Someone walked out. Num people: " + numPeople);
+void enabledPeriodic()
+{
+    if (debounce > 0)
+    {
+        debounce--;
+    }
+    else
+    {
+        // No item has triggered the detecting in/out process
+        int beforeDoorDistance = querySonar(BEFORE_DOOR);
+        int afterDoorDistance = querySonar(AFTER_DOOR);
 
-  if (numPeople == 0) {
-    currentState = TURNING_OFF;
-  }
+        bool beforeDoorInRange = isInRange(beforeDoorDistance);
+        bool afterDoorInRange = isInRange(afterDoorDistance);
+
+        if (beforeDoorInRange && afterDoorInRange)
+        {
+            currentSonarState = BOTH_DETECTED;
+        }
+        else if (beforeDoorInRange)
+        {
+            currentSonarState = BEFORE_DETECTED;
+        }
+        else if (afterDoorInRange)
+        {
+            currentSonarState = AFTER_DETECTED;
+        }
+        else
+        {
+            currentSonarState = NONE_DETECTED;
+        }
+
+        addToDetectionHistory(currentSonarState);
+
+        addToSonarHistory(getDetected());
+
+        Serial.print(sonarHistory[0]);
+        Serial.print(" ");
+        Serial.print(sonarHistory[1]);
+        Serial.print(" ");
+        Serial.print(sonarHistory[2]);
+        Serial.print(" ");
+        Serial.println(sonarHistory[3]);
+
+        if (matchSonarHistoryPattern(walkInPattern) || matchSonarHistoryPattern(altWalkInPattern))
+        {
+            walkIn();
+            debounce = ticksToDebounce;
+            resetSonarHistory();
+        }
+        else if (matchSonarHistoryPattern(walkOutPattern) || matchSonarHistoryPattern(altWalkOutPattern))
+        {
+            walkOut();
+            debounce = ticksToDebounce;
+            resetSonarHistory();
+        }
+    }
+}
+
+void onPeriodic()
+{
+}
+
+void offPeriodic()
+{
+}
+
+void turningOnPeriodic()
+{
+    pixels.setBrightness(turnOn[currentAnimationTick]);
+    pixels.show();
+
+    currentAnimationTick++;
+
+    if (currentAnimationTick > sizeof(turnOn) / sizeof(int))
+    {
+        currentState = ON;
+    }
+}
+
+void turningOffPeriodic()
+{
+    pixels.setBrightness(turnOff[currentAnimationTick]);
+    pixels.show();
+
+    currentAnimationTick++;
+
+    if (currentAnimationTick > sizeof(turnOff) / sizeof(int))
+    {
+        currentState = OFF;
+    }
+}
+
+void walkIn()
+{
+    if (numPeople == 0)
+    {
+        currentState = TURNING_ON;
+    }
+
+    numPeople++;
+    Serial.print("In: ");
+    Serial.println(numPeople);
+}
+
+void addToSonarHistory(sonarState state)
+{
+    if (sonarHistory[3] == state)
+        return;
+
+    sonarHistory[0] = sonarHistory[1];
+    sonarHistory[1] = sonarHistory[2];
+    sonarHistory[2] = sonarHistory[3];
+    sonarHistory[3] = state;
+}
+
+void addToDetectionHistory(sonarState state)
+{
+    detectionHistory[0] = detectionHistory[1];
+    detectionHistory[1] = detectionHistory[2];
+    detectionHistory[2] = state;
+}
+
+sonarState getDetected()
+{
+    if (detectionHistory[0] == detectionHistory[1] && detectionHistory[0] == detectionHistory[2])
+    {
+        return detectionHistory[0];
+    }
+    else
+    {
+        return sonarHistory[3];
+    }
+}
+
+bool matchSonarHistoryPattern(sonarState match[4])
+{
+    return match[0] == sonarHistory[0] && match[1] == sonarHistory[1] && match[2] == sonarHistory[2] && match[3] == sonarHistory[3];
+}
+
+void resetSonarHistory()
+{
+    sonarHistory[0] = NONE_DETECTED;
+    sonarHistory[1] = NONE_DETECTED;
+    sonarHistory[2] = NONE_DETECTED;
+    sonarHistory[3] = NONE_DETECTED;
+}
+
+void walkOut()
+{
+    numPeople--;
+    Serial.print("Out: ");
+    Serial.println(numPeople);
+
+    if (numPeople == 0)
+    {
+        currentState = TURNING_OFF;
+    }
+}
+
+int querySonar(enum sonar sensor)
+{
+    int trigPin;
+    int echoPin;
+
+    if (sensor == BEFORE_DOOR)
+    {
+        trigPin = SONAR_ONE_TRIG_PIN;
+        echoPin = SONAR_ONE_ECHO_PIN;
+    }
+    else if (sensor == AFTER_DOOR)
+    {
+        trigPin = SONAR_TWO_TRIG_PIN;
+        echoPin = SONAR_TWO_ECHO_PIN;
+    }
+
+    // Clears the trigger
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    // Triggers sonar
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    // Measure time for ping to return
+    unsigned long duration = pulseIn(echoPin, HIGH);
+
+    // Does the math!!! crazy hard LOL!!!
+    return duration * 0.034 / 2;
+}
+
+bool isInRange(int distance)
+{
+    return distance > 1 && distance <= 30;
 }
