1e75f24e1195bb85b25042b06644ebbd860b5eaf
Reza Bagheri <rezq.bagheri@gmail.com>
2023-06-17 20:31:28 -0700

    Updated to work with Firebase. New Firebase library too.

--- RezqRoom.ino
+++ RezqRoom.ino
@@ -1,23 +1,59 @@
+// Try this: https://gist.github.com/adi-g15/de41e96079a5b63045e86dc7c8c5c87e // to connect to ESP8266 when you actually get it idk
 #include <Arduino.h>
-#include <FirebaseArduino.h>
+#include <FirebaseESP8266.h>
 #include <ESP8266WiFi.h>
 
-#include "ArrayUtils.h"
 #include "Keys.h"
 
-const char* PROFILE = "BB8D7706-CB83-4339-918F-0E371D0AA36B";
+const char* ROOM = "BB8D7706-CB83-4339-918F-0E371D0AA36B";
 
 const int SENSOR_PIN = A0;
+
+enum RoomFeatures {
+  NO_FEATURE=0b00000, LEDs=0b00001
+};
+
+enum LEDStatus {
+  OFF=0,
+  WHITE=1
+};
+
+struct RoomConfig {
+  int featuresBitmask;
+  RoomFeatures features[5];
+  LEDStatus ledStatus;
+} room;
 
 void setup() {
   Serial.begin(115200);
 
-  Serial.println("Initializing");
+  Serial.println("Configuring pins");
 
   pinMode(SENSOR_PIN, INPUT);
 
-  Serial.println("Pins configured");
+  Serial.println("Done configuring pins");
 
+  printSeperationLine();
+
+  wifiReconnect();
+
+  firebaseReconnect();
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Lost connection to ");
+    Serial.println(SSID_);
+    wifiReconnect();
+  }
+
+  // Query Firebase and update local config
+  configureFromDB(&room);
+
+  delay(500); // 0.5s loop times (ideal. Assumes not blocked by trying to connect to wifi)
+}
+
+void wifiReconnect() {
   Serial.print("Connecting to ");
   Serial.print(SSID_);
   Serial.print(" ");
@@ -28,11 +64,11 @@ void setup() {
 
   int i = 0;
   while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    yield(); // Allow ESP8266 background operations
     delay(1000);
     i++;
     Serial.print(".");
-  }
-  Serial.print("\nSuccessfully connected to ");
+  } Serial.print("\nSuccessfully connected to ");
   Serial.print(SSID_);
   Serial.print(" with IP ");
   Serial.print(WiFi.localIP());
@@ -40,31 +76,54 @@ void setup() {
   Serial.print(i);
   Serial.println(" seconds)");
 
-  firebaseReconnect();
+  printSeperationLine();
 }
 
-void loop() {
-  // Query Firebase and update local config
-  FirebaseObject remoteConfigGet = Firebase.get(String("/profiles/") + PROFILE);
-  if (Firebase.failed()) {
-      Serial.println("Firebase get failed");
-      Serial.println(Firebase.error());
-  } else {
-    config.enabled = remoteConfigGet.getBool("enabled");
-  }
-  
-  delay(250);
-}
-
+void firebaseReconnect() {
   Serial.println("Initializing Firebase connection");
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Serial.println("Firebase done initializing");
+  FirebaseAuth auth;
+
+  // Define the FirebaseConfig data for config data
+  FirebaseConfig config;
+
+  // Assign the project host and api key (required)
+  config.host = FIREBASE_HOST;
+
+  config.api_key = FIREBASE_AUTH;
+
+  Firebase.begin(&config, &auth);
+
+  Serial.println("Done initializing Firebase connection");
+
+  printSeperationLine();
 }
 
-void printConfig(Config *config) {
-  Serial.println("Config:");
-  Serial.println(" - Using Purely Interval: " + String(config->usingPurelyInterval));
-  Serial.println(" - Interval: " + String(config->interval));
-  Serial.println(" - Watering Threshold: " + String(config->wateringThreshold));
-  Serial.println(" - Watering Time: " + String(config->wateringTime));
+void configureFromDB(RoomConfig * config) {
+  FirebaseData firebaseData;
+
+  for (int i = 0; i < 5; i++) {
+    config->features[i] = NO_FEATURE;
+  }
+
+  if (Firebase.getInt(firebaseData, "features") && firebaseData.dataTypeEnum() == fb_esp_rtdb_data_type_integer) {
+    config->featuresBitmask = firebaseData.to<int>();
+
+    // Check what features the featuresBitmask (bit representation of all the 5 possible features) stores
+    // Compare it to a bitmask of all possible features and add them to the feature list of they exist
+    int indexWritten = 0;
+    if (config->featuresBitmask & LEDs) {
+      config->features[indexWritten] = LEDs;
+      indexWritten++;
+    }
+  }
+
+  if (Firebase.getInt(firebaseData, "ledStatus") && firebaseData.dataTypeEnum() == fb_esp_rtdb_data_type_integer) {
+    config->ledStatus = static_cast<LEDStatus>(firebaseData.to<int>());
+  } else {
+    config->ledStatus = OFF;
+  }
+}
+
+void printSeperationLine() {
+  Serial.println("------------------------------");
 }
